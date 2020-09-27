37ebf9327e64a2c4855f38393067ef52be1a4731
hikarut <ht.hikaru.takahashi@gmail.com>
2020-09-27 12:43:06 +0900

    [add] 404 page

---
+++ pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <p class="has-text-centered">Error</p>
+        <p class="has-text-centered error-message">
+          <nuxt-link to="/">Top</nuxt-link>に戻る
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.error-message {
+  margin-top: 10px;
+}
+</style>
