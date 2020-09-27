713f243c3903a37132af515d44b9c5c988c91527
hikarut <ht.hikaru.takahashi@gmail.com>
2020-09-27 18:06:02 +0900

    [add] error page

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <p class="has-text-centered">Error</p>
+        <p class="has-text-centered error-message">
+          <nuxt-link to="/">Top</nuxt-link>に戻る
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.error-message {
+  margin-top: 10px;
+}
+</style>
