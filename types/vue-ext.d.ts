cc898160eaa9ade082d2ce21685b3fba0d62850e
hikarut <ht.hikaru.takahashi@gmail.com>
2024-06-08 19:56:15 +0900

    [fix] config

--- types/vue-ext.d.ts
+++ types/vue-ext.d.ts
@@ -6,10 +6,4 @@ declare module '*.js' {
 declare module 'jstat' {
   // const value: any
   // export default value
-  
-  // export const jStat: {
-  //   normal: {
-  //     cdf: (value: number, mean: number, standardDeviation: number) => number;
-  //   };
-  // };
 }
